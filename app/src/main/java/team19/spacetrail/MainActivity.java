bf63bdf2f2ba98acb51bb1e3ae2e57a22690c665
Andrew Chepey <achepey@gmail.com>
2014-11-14 21:39:06 -0600

    Added some functionality to the new game menu

--- app/src/main/java/team19/spacetrail/MainActivity.java
+++ app/src/main/java/team19/spacetrail/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +12,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void newGameMenu(View view) {
+        setContentView(R.layout.new_game);
     }
 
 
