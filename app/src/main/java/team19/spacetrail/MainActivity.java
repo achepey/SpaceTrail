738720968546d52c1bf927c29228f15120fcde2a
Andrew Chepey <achepey@gmail.com>
2014-11-15 13:15:24 -0600

    Adding transition to space activity

--- app/src/main/java/team19/spacetrail/MainActivity.java
+++ app/src/main/java/team19/spacetrail/MainActivity.java
@@ -1,12 +1,16 @@
 package team19.spacetrail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -30,6 +34,36 @@ public class MainActivity extends Activity {
 
     public void loadGameMenu(View view) {
         setContentView(R.layout.activity_load_game);
+    }
+
+    public void buyResources() {
+        //This is where we will change the xml file to reflect the changes in resources.
+        Button buy = (Button) findViewById(R.id.buyButton);
+        EditText fuel = (EditText) findViewById(R.id.fuelTextField);
+        EditText food = (EditText) findViewById(R.id.foodTextField);
+        EditText engine = (EditText) findViewById(R.id.enginePartTextField);
+        EditText aluminum = (EditText) findViewById(R.id.aluminumPartsTextField);
+        EditText wings = (EditText) findViewById(R.id.wingTextField);
+    }
+    /* Called when the user hits the send to space button */
+    public void headToSpace(View view) {
+        Intent intent = new Intent(this, SpaceActivity.class);
+        TextView fuel = (TextView) findViewById(R.id.fuelQuantity);
+        int fuelint = Integer.parseInt(fuel.getText().toString());
+
+        TextView food = (TextView) findViewById(R.id.foodQuantity);
+        int foodint = Integer.parseInt(fuel.getText().toString());
+
+        TextView engine = (TextView) findViewById(R.id.engineQuantity);
+        int engineint = Integer.parseInt(fuel.getText().toString());
+
+        TextView alum = (TextView) findViewById(R.id.alumQuantity);
+        int alumint = Integer.parseInt(fuel.getText().toString());
+
+        TextView wings = (TextView) findViewById(R.id.wingQuantity);
+        int wingsint = Integer.parseInt(fuel.getText().toString());
+
+        startActivity(intent);
     }
 
     @Override
